9c8f31c17de78dc94d59a30267ee64a252f3bb49
khasanjon <khasanjon-dev@gmail.com>
2024-05-19 00:51:05 +0500

    r

--- main1.py
+++ main1.py
@@ -39,7 +39,7 @@ def wrap_text_to_width(draw, text, font, max_width):
     return lines
 
 
-def coupons(names: list, certificate: str, channel_name: str, font_path: str):
+def generate_certificate(names: list, certificate: str, channel_name: str, font_path: str):
     if not os.path.exists("certificates"):
         os.makedirs("certificates")
 
@@ -53,6 +53,7 @@ def coupons(names: list, certificate: str, channel_name: str, font_path: str):
         percentage = 80
         author_name = 'Malika Rustamova'
         channel_name = 'devmasters'
+        # TODO ... fix values
         text = (
             f"Siz {channel_name.upper()} - jamoasi tomonidan o'tqazilgan testda {count_test} ta savoldan {count_test} "
             f"tasiga to'g'ri javob berib,"
@@ -64,28 +65,28 @@ def coupons(names: list, certificate: str, channel_name: str, font_path: str):
 
         # writing name
         name_size = 120
-        font_name = ImageFont.truetype(font_path, name_size)
-        font_name = fit_text_to_width(draw, name, font_name, image_width - 40)
-        text_bbox = draw.textbbox((0, 0), name, font=font_name)
-        text_w = text_bbox[2] - text_bbox[0]
-        text_h = text_bbox[3] - text_bbox[1]
+        name_font = ImageFont.truetype(font_path, name_size)
+        name_font = fit_text_to_width(draw, name, name_font, image_width - 40)
+        name_bbox = draw.textbbox((0, 0), name, font=name_font)
+        text_w = name_bbox[2] - name_bbox[0]
+        text_h = name_bbox[3] - name_bbox[1]
         text_x_position = (image_width - text_w) / 2
         text_y_position = (image_height - text_h) / 2 - 100
-        draw.text((text_x_position, text_y_position), name, font=font_name, fill=(134, 91, 52))
+        draw.text((text_x_position, text_y_position), name, font=name_font, fill=(134, 91, 52))
 
         # writing text
         text_size = 35
-        font_text = ImageFont.truetype(font_channel_path, text_size)
+        text_font = ImageFont.truetype(font_channel_path, text_size)
         wrapped_text = []
         for line in text:
-            wrapped_lines = wrap_text_to_width(draw, line, font_text, image_width - 40)
+            wrapped_lines = wrap_text_to_width(draw, line, text_font, image_width - 40)
             wrapped_text.extend(wrapped_lines)
         total_text_height = sum(
-            draw.textbbox((0, 0), line, font=font_text)[3] - draw.textbbox((0, 0), line, font=font_text)[1] for line in
+            draw.textbbox((0, 0), line, font=text_font)[3] - draw.textbbox((0, 0), line, font=text_font)[1] for line in
             wrapped_text)
         y = (image_height - total_text_height) / 2 + 80
         for line in wrapped_text:
-            font_text = fit_text_to_width(draw, line, font_text, image_width - 40)
+            font_text = fit_text_to_width(draw, line, text_font, image_width - 40)
             bbox = draw.textbbox((0, 0), line, font=font_text)
             text_w = bbox[2] - bbox[0]
             text_h = bbox[3] - bbox[1]
@@ -105,23 +106,35 @@ def coupons(names: list, certificate: str, channel_name: str, font_path: str):
         text_y_position = (image_height - text_h) / 2 + 365
         draw.text((text_x_position, text_y_position), author_name, font=font_author, fill=(134, 91, 52))
 
+        # writing bot name
+        bot_size = 40
+        bot_font_path = 'fonts/text-font-2.otf'
+        bot_text = '#exam_e_bot'
+        bot_font = ImageFont.truetype(bot_font_path, bot_size)
+        bot_bbox = draw.textbbox((0, 0), bot_text, font=bot_font)
+        text_w = bot_bbox[2] - bot_bbox[0]
+        text_h = bot_bbox[3] - bot_bbox[1]
+        text_x_position = (image_width - text_w) / 2 + 395
+        text_y_position = (image_height - text_h) / 2 + 365
+        draw.text((text_x_position, text_y_position), bot_text, font=bot_font, fill=(134, 91, 52))
+
         # writing rank
-        rank_size = 30
+        rank_size = 35
         rank_font_path = 'fonts/text-font-2.otf'
-        rank_number = 120
+        rank_number = 1000
         rank_text = 'Reyting: TOP #' + str(rank_number)
         rank_font = ImageFont.truetype(rank_font_path, rank_size)
         rank_bbox = draw.textbbox((0, 0), rank_text, font=rank_font)
         text_w = rank_bbox[2] - rank_bbox[0]
         text_h = rank_bbox[3] - rank_bbox[1]
         text_x_position = (image_width - text_w) / 2
-        text_y_position = (image_height - text_h) / 2 + 527
+        text_y_position = (image_height - text_h) / 2 + 515
         draw.text((text_x_position, text_y_position), rank_text, font=rank_font, fill=(134, 91, 52))
 
         img.save(os.path.join("certificates", "{}.png".format(name)))
 
 
 FONT = "fonts/font5.otf"
-CERTIFICATE = "templates/template2.png"
+CERTIFICATE = "templates/template.png"
 NAMES = ['Hasanjon Mamadaliyev']
 coupons(NAMES, CERTIFICATE, 'devmasters', FONT)
